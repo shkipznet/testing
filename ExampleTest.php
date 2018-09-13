f756a658635aa798cfb7c9677607e8d3232252b4
Alexandr Pavlenko <a.pavlenko@bintime.com>
2018-09-13 12:55:56 +0300

    first commit

---
+++ ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testExample(): void
+    {
+        $var = true;
+
+        $this->assertEquals(true, $var);
+    }
+}
