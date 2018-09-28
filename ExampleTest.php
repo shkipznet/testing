6e8501618a8bcb7aa3c68df89b4ab9820689103c
shkipznet <43667184+shkipznet@users.noreply.github.com>
2018-09-28 10:50:48 +0300

    LIST-1 Add comments

--- ExampleTest.php
+++ ExampleTest.php
@@ -5,6 +5,9 @@ use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
 {
+    /**
+    * @return void
+    */
     public function testExample(): void
     {
         $this->assertEquals(true, false);
